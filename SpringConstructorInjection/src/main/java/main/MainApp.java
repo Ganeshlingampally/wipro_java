94e21414ad544af01433042e166db5158f237673
Ganeshlingampally <ganeshlingampally247@gmail.com>
2025-08-04 00:24:49 +0530

    init commit

---
+++ SpringConstructorInjection/src/main/java/main/MainApp.java
@@ -0,0 +1,13 @@
+package main;
+
+import com.example.model.Account;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Account account = context.getBean("account", Account.class);
+        account.displayAccountDetails();
+    }
+}
